b266920ab1b65c50b6e3e7388eeef09133111a5f
Rasmus Hilmar <142843432+RasmusHilmar1@users.noreply.github.com>
2025-11-21 08:57:39 +0100

    Fix base URL fallback in error logging (#17)
    Removed trailing slash from base URL fallback.

--- app/group3/errorLogging.tsx
+++ app/group3/errorLogging.tsx
@@ -19,7 +19,7 @@ export const log = {
                     message: typeof error === 'string' ? error : JSON.stringify(error),
                 };
 
-                const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://translator-service.cs-25-sw-5-03.svc.cluster.local/'; // Delete fallback when deployed
+                const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://translator-service.cs-25-sw-5-03.svc.cluster.local'; // Delete fallback when deployed
                 await fetch(`${baseUrl}/api/logs/frontend`, {
                     method: 'POST',
                     headers: {
